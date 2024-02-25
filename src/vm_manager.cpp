dea806d16a581b184c935a7f9dbb4d21a80fc10e
YS0meone <timyuan1016@gmail.com>
2024-02-25 14:11:20 -0800

    Finished the demanding page feature and passes the sample test

--- src/vm_manager.cpp
+++ src/vm_manager.cpp
@@ -18,6 +18,10 @@ VMManager::VMManager(const std::string& initFilePath, const std::string& inputFi
     initFile.open(initFilePath.c_str());
     inputFile.open(inputFilePath.c_str());
     outputFile.open(outputFilePath.c_str());
+    for (int i = 2; i < NUM_OF_PAGES; ++i) {
+        // initialize all free frames
+        freeFrames.push_back(i);
+    }
     initializePM();
 }
 
@@ -38,6 +42,12 @@ void VMManager::initializePM() {
     std::istringstream iss1(line1);
     // initialize the ST part
     while (iss1 >> segmentID >> segmentSize >> ptFrame) {
+        // use the frame for the PT
+        // remove it from the freeFrames
+        if (ptFrame > 0) {
+            freeFrames.remove(ptFrame);
+        }
+        // ST would always be in the PM. Frame 0, 1
         setSegmentTable(segmentID, segmentSize, ptFrame);
     }
 
@@ -50,6 +60,12 @@ void VMManager::initializePM() {
     std::istringstream iss2(line2);
     // initialize the PT part
     while (iss2 >> segmentID >> pageOffset >> frameID) {
+        // check if the frame in on PM
+        // if so remove the free frame
+        if (frameID > 0) {
+            freeFrames.remove(frameID);
+        }
+        // page table might be on the disk
         setPageTable(segmentID, pageOffset, frameID);
     }
 
@@ -65,12 +81,20 @@ void VMManager::printST() {
 
 
 void VMManager::setSegmentTable(int segmentID, int segmentSize, int ptFrame) {
+
     pm[2 * segmentID] = segmentSize;
     pm[2 * segmentID + 1] = ptFrame;
 }
 
 void VMManager::setPageTable(int segmentID, int pageOffset, int frameID) {
-    pm[pm[2 * segmentID + 1] * PAGE_SIZE + pageOffset] = frameID;
+    // check if the pageTable is on disk
+    int ptFrame = pm[2 * segmentID + 1];
+    if (ptFrame < 0){
+        pd[-ptFrame][pageOffset] = frameID;
+    }
+    else{
+        pm[pm[2 * segmentID + 1] * PAGE_SIZE + pageOffset] = frameID;
+    }
 }
 
 void VMManager::printPT(int segmentID) {
@@ -93,6 +117,22 @@ int VMManager::translateVA(int virtualAddress) {
         return -1;
 //        throw std::invalid_argument("Try to access beyond the segment size!");
     }
+    int ptFrame = pm[2 * s + 1];
+    // the page table is not on disk
+    if (ptFrame < 0) {
+        // allocate the lowest frame for the page table
+        int pmFreeFrame = freeFrames.front();
+        freeFrames.pop_front();
+        readBlock(-ptFrame, pmFreeFrame * PAGE_SIZE);
+        pm[2 * s + 1] = pmFreeFrame;
+    }
+    int destFrame = pm[pm[2 * s + 1] * PAGE_SIZE + p];
+    if (destFrame < 0) {
+        int pmFreeFrame = freeFrames.front();
+        freeFrames.pop_front();
+        readBlock(-destFrame, pmFreeFrame * PAGE_SIZE);
+        pm[pm[2 * s + 1] * PAGE_SIZE + p] = pmFreeFrame;
+    }
     int pa = pm[pm[2 * s + 1] * PAGE_SIZE + p] * PAGE_SIZE + w;
     return pa;
 }
@@ -102,5 +142,10 @@ void VMManager::translate() {
     while (inputFile >> virtualAddress){
         outputFile << translateVA(virtualAddress) << " ";
     }
+}
+
+void VMManager::readBlock(int blockID, int pmDest) {
+    // move the block from paged disk
+    std::copy(pd[blockID], pd[blockID] + PAGE_SIZE, pm + pmDest);
 }
 
